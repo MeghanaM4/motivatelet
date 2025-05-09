cf3c47ddd11b2c706aeb189848294a481ade62cd
Meghana Madiraju <meghanambpd@gmail.com>
2025-05-09 16:59:35 -0400

    minifying

--- motivatelet.js
+++ motivatelet.js
@@ -1,11 +1,10 @@
 javascript:(function() {
-    reminders = ["getting a 5 is easy, lock in", "your physics teacher didn't get grey hairs just for you to fail",
+    m = ["getting a 5 is easy, lock in", "your physics teacher didn't get grey hairs just for you to fail",
         "the only L you're gonna get is angular momentum", "study unit 6, rotational inertia isn't gonna derive itself",
         "you are NOT an F student", "show gauss who's boss. no 17th century old man is going to best you", 
         "induction who?", "at this point, you're too far in.", "home stretch man, you will not die because of capacitance",
         "if you had to linearize your physics skill it would be slope = awesomeness", 
         "experimental design procedure has nothing on your study habits. minimize that uncertainty."];
-    var msgInd = Math.floor(Math.random() * (reminders.length - 1));
-    console.log(msgInd + ": " + reminders[msgInd]);
-    alert(reminders[msgInd]);
+    var i=Math.floor(Math.random()*(m.length-1));
+    alert(r[i]);
 })();
